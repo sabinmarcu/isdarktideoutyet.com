7f5611b0a445b06030a030fa07edd76b102ce234
Sabin Marcu <sabinmarcu@gmail.com>
2021-07-02 19:14:02 +0300

    first iteration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import img from './assets/e93.jpg';
+
+console.log(img)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="background">
+        <img src={img} alt="status-bg" />
+      </div>
+      <div className="foreground">
+        <img src={img} alt="status" />
+      </div>
+    </>
   );
 }
 
